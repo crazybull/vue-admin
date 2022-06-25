06ad44e1a442cf3423a3aa653ab6c3e537a93dc9
crazybull_mac <626066708@qq.com>
2022-06-25 22:59:20 +0800

    初始化的一些代码

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    <el-button type="primary" >Primary</el-button>
+  </div>
+</template>
+<style lang="scss" scoped>
+    #login{
+        background:#344a5f;
+        height: 100vh;
+        color:#fff;
+    }
+</style>
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Login"
+};
+</script>
